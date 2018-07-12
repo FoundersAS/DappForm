538cdcc97721989463030186636fe07b64c15de3
Julian <julian@Madss-MacBook-Pro.local>
2018-07-12 12:58:25 +0200

    first

---
+++ src/main.js
@@ -0,0 +1,27 @@
+import { getFile, putFile } from './write.js';
+const { blockstack } = window;
+if (blockstack.isUserSignedIn()) {
+    // const userData = blockstack.loadUserData()
+    // const user = new blockstack.Person(this.userData.profile)
+    // const user.username = this.userData.username
+    main();
+}
+else if (blockstack.isSignInPending()) {
+    blockstack.handlePendingSignIn()
+        .then((userData) => {
+        console.log('signed in!');
+        console.log(userData);
+        window.location.reload();
+    })
+        .catch((err) => console.error(err));
+}
+else {
+    blockstack.redirectToSignIn();
+}
+async function main() {
+    const name = 'tmp/' + 'junk' + new Date().getTime();
+    await putFile(name, { random: Math.random() });
+    console.log('Wrote some junk!');
+    const retrieved = await getFile(name);
+    console.log('got', retrieved);
+}
