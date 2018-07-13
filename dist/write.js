51f15ee37e1b127752ab122b8e7d273046e3c5a2
julian <github2018@studio42.dk>
2018-07-13 11:05:41 +0200

    remove build files

--- dist/write.js
+++
@@ -1,8 +0,0 @@
-const { blockstack } = window; // hax
-export async function putFile(path, contents) {
-    await blockstack.putFile(path, JSON.stringify(contents));
-}
-export async function getFile(path) {
-    const contents = await blockstack.getFile(path);
-    return JSON.parse(contents);
-}
