a21eb60dd94e8bc362919f10875911af4b90f18f
julian <github2018@studio42.dk>
2018-07-17 13:59:49 +0200

    build form route

--- src/components/builder/builder.ts
+++ src/components/builder/builder.ts
@@ -1,19 +1,118 @@
-import { html } from '../../../node_modules/lit-html/src/lit-html'
+import { render, html } from '../../../node_modules/lit-html/lib/lit-extended'
 import { Form, Question } from '../../form-format'
 const uuidv4 = require('../../../node_modules/uuid/v4')
+const {blockstack} = window as any
+
+const questions = <Question[]>[]
 
 export function update() {
+  const el:HTMLElement = document.querySelector(`build-form`)
+  const questionsListTpl = questions.map(renderLeaf)
 
+  const tpl = html`
+<h3>Build</h3>
+
+<form class="grid-x grid-margin-y grid-margin-x">
+  <div class="cell small-6">
+    <label>
+        Human-readable name
+        <input type="text" name="form-name">    
+    </label>
+  </div>
+  <div class="cell small-6">
+    <label>
+        Intro text
+        <input type="text" name="intro-text">    
+    </label>
+  </div>
+  
+  <div class="cell small-12">
+    ${questionsListTpl}
+  </div>
+    
+  <div class="cell small-12">  
+    <div class="grid-x">
+      <div class="cell auto">
+          <button on-click="${() => {addField(questions); update()}}" class="hollow button" type="button">Add text</button>      
+      </div>
+      <div class="cell shrink">
+          <button on-click="${() => collectForm()}" class="hollow button primary" type="button">Save</button>      
+      </div> 
+    </div>
+  </div>   
+</form>  
+`
+
+  render(tpl, el)
 }
 
-function addField (name:string, form:Form) {
+function collectForm () {
+  const newFrom = <Form>{
+    uuid: uuidv4(),
+    created: new Date(),
+    modified: new Date(),
+    questions: [],
+    name: document.querySelector('.form-name')
+  }
+  // questions.length
+  const questions = Array.from(document.querySelectorAll('.question-item'))
+    .map((el:HTMLElement) => {
+      return <Question>{
+        uuid: uuidv4()
+      }
+    })
+  // question-item
+}
+
+function addField (questions:Question[]) {
   const q = <Question>{
-    name: '',
     uuid: uuidv4()
   }
-  form.questions.push(q)
+  questions.push(q)
 }
 
-function renderLeaf(type:string) {
-  return html`<input type='${type}'>`
+function renderLeaf(q:Question) {
+  return html`
+<div class="grid-x grid-margin-x grid-margin-y question-item"> 
+  <div class="cell small-8"> 
+    <label>Question label
+      <input type='text' name="${q.name}" placeholder="Question label">   
+    </label>
+  </div>
+  <div class="cell small-4">
+    <label>Type
+      <select name="${q.name}-q-type">
+          <option>Text</option>
+          <option>Number</option>
+      </select>
+    </label>
+  </div>
+</div>
+  `
+}
+
+async function uploadShare (newForm:Form) {
+  // console.assert(blockstack.isUserSignedIn(), "User is not logged id")
+  // const authorPubkey = blockstack.getPublicKeyFromPrivate( blockstack.loadUserData().appPrivateKey )
+  // const recipientPubKey = '0304eb59f9d33acdc46825c160405b1154ccabfff226fb777e4ce5df4c8f8cacd4'
+
+  // const quickForm = {
+  //   id: 43,
+  //   name: "The real questions.",
+  //   questions: [
+  //     {label: "Do you like privacy?"},
+  //   ],
+  //   submissions: <Object[]>[],
+  // }
+  // const signedPath = signMessage('/forms', blockstack.loadUserData().appPrivateKey)
+  // await putFile(`forms/${quickForm.id}.json`, quickForm)
+
+  await blockstack.putFile(`forms/${newForm.uuid}.json`, JSON.stringify(newForm), {encrypt: false})
+
+  // Object.values(blockstack.loadUserData().profile.apps)[0]
+  // lookupProfile
+
+  // target to find: https://gaia.blockstack.org/hub/14ktrFjBTrQhmvZYdEgVZPEvceo6uKiyLZ/forms/43.json
+  // where the hash is the app public address
+  console.debug(`did put stuff`)
 }
--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,10 @@ function main () {
   Store.setRouteAction( parseInt(sessionStorage.route, 10) || Route.Login )
 
   // nav
-  document.querySelector('.nav-item-list').addEventListener('click', () => Store.setRouteAction(Route.FormsList))
-  document.querySelector('.button-signout').addEventListener('click', () => blockstackSignout())
+  const nav = document.querySelector(`nav`)
+  nav.querySelector('.button-list').addEventListener('click', () => Store.setRouteAction(Route.FormsList))
+  nav.querySelector('.button-signout').addEventListener('click', () => blockstackSignout())
+  nav.querySelector('.button-build').addEventListener('click', () => Store.setRouteAction(Route.Build))
 }
 
 async function uploadEncrypt () {
@@ -57,32 +59,6 @@ async function uploadEncrypt () {
 }
 (window as any).upload = uploadEncrypt
 
-async function uploadShare () {
-  console.assert(blockstack.isUserSignedIn(), "User is not logged id")
-  const authorPubkey = blockstack.getPublicKeyFromPrivate( blockstack.loadUserData().appPrivateKey )
-  const recipientPubKey = '0304eb59f9d33acdc46825c160405b1154ccabfff226fb777e4ce5df4c8f8cacd4'
-
-  const quickForm = {
-    id: 43,
-    name: "The real questions.",
-    questions: [
-      {label: "Do you like privacy?"},
-    ],
-    submissions: <Object[]>[],
-  }
-
-  // const signedPath = signMessage('/forms', blockstack.loadUserData().appPrivateKey)
-  // await putFile(`forms/${quickForm.id}.json`, quickForm)
-  await blockstack.putFile(`forms/${quickForm.id}.json`, JSON.stringify(quickForm), {encrypt: false})
-
-  // Object.values(blockstack.loadUserData().profile.apps)[0]
-
-  // lookupProfile
-  // target to find: https://gaia.blockstack.org/hub/14ktrFjBTrQhmvZYdEgVZPEvceo6uKiyLZ/forms/43.json
-  // where the hash is the app public address
-  console.debug(`did put stuff`)
-}
-(window as any).share = uploadShare
 
 
 // side effects
