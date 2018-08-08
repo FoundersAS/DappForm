c0265234f0a1d561a7efe73b24bce6db49d59348
Joshua Karjala-Svendsen <joshua@fluxuries.com>
2018-08-08 16:58:28 +0200

    removing some stuff

--- src/components/view-form/view-form.ts
+++ src/components/view-form/view-form.ts
@@ -1,9 +1,9 @@
 import { html, render } from '../../../node_modules/lit-html/lib/lit-extended'
 import Store from '../../store'
 import { Route } from '../router'
-import { Form, deleteForm, getForm, getFormSubmissions, saveForm } from 'dappform-forms-api'
+import { deleteForm, getForm, getFormSubmissions } from 'dappform-forms-api'
 import BlockstackUtils from '../../util/blockstackUtils'
-import { generateReport, weeklyStats } from './weekly-stats'
+import { weeklyStats } from './weekly-stats'
 
 export async function update () {
   const el = document.querySelector('forms-view')
@@ -18,24 +18,6 @@ export async function update () {
 
   const submissions = await getFormSubmissions(uuid)
   const {lastWeek, total} = weeklyStats(Object.values(submissions))
-
-  const toggleReporting = async (form:Form) => {
-    if (form.weeklyReportRecipient) {
-      delete form.weeklyReportRecipient
-    }
-    else {
-      const email = (el.querySelector('input[name="report-email"]') as HTMLInputElement).value
-      form.weeklyReportRecipient = email
-    }
-    await saveForm(form)
-    update()
-  }
-
-  const generateReportHandler = (form:Form):Promise<void> => {
-    const postmarkFrom = (el.querySelector('[name="report-email"]') as HTMLInputElement).value
-    const postmarkKey = (el.querySelector('[name="postmark-key"]') as HTMLInputElement).value
-    return generateReport(form, postmarkFrom, postmarkKey)
-  }
 
   const tpl = html`
   <h3>Form dashboard <em>${form.name}</em></h3>
@@ -65,28 +47,6 @@ export async function update () {
       <p><button class="clear button link" on-click="${() => Store.setRouteAction(Route.SubmissionsView, { formId: uuid }) }">View Submissions</button>
 
       <p>${lastWeek} submissions last week. Total ${total}.</p>
-
-      <h5>Email reports</h5>
-      <form>     
-        <div>        
-          <label>Recipient email
-          <input placeholder="Email address" type="email" value="${form.weeklyReportRecipient}" name="report-email">
-          </label>
-        </div>
-      
-        <div>
-          <label>
-              Postmark API key
-              <input placeholder="Postmark API key" type="text" name="postmark-key">
-          </label>
-        </div>
-        
-        <div>
-          <button class="button hide" on-click="${(evt:Event) => handleAsyncButton(evt, toggleReporting(form))}" type="button">${(form as Form).weeklyReportRecipient ? 'Dis':'En'}able weekly report</button>
-          <button class="button" on-click="${(evt:Event) => handleAsyncButton(evt, generateReportHandler(form)) }" type="button">Build report now</button>
-        </div>
-      </form>
-
     </div>
 
   </div>
