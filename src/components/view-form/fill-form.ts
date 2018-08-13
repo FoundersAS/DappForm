639cf2b48c5502e9f2e33ec4b96969669d8c0ec6
julian <github2018@studio42.dk>
2018-08-13 14:49:45 +0200

    use fill-form module for rendering

--- src/components/view-form/fill-form.ts
+++ src/components/view-form/fill-form.ts
@@ -1,8 +1,8 @@
-import { html, render } from '../../../node_modules/lit-html/lib/lit-extended'
-import { Answer, Form, Submission, getForm, getPublishPath } from 'dappform-forms-api'
+import { Answer, Form, getForm, getPublishPath, Submission } from 'dappform-forms-api'
 import Store from '../../store'
 import { v4 as uuid } from 'uuid'
 import { encryptFile } from '../../util/crypto'
+import {renderForm} from '../../../node_modules/dappform-filler/index'
 
 const blockstack = require('blockstack')
 
@@ -27,40 +27,18 @@ export async function update () {
     if (res.status === 200) {
       const json = await res.json()
       form = json
+      console.assert(form, `Form was not found at ${pathToPublicForm}`)
     }
   }
   else if (submission) {
     formUuid = submission.formUuid
     form = await getForm(formUuid)
+    console.assert(form, `Form was not found by getForm(${formUuid})`)
   }
 
-  const questions = ((!form) ? [] : form.questions)
-    .map(q => {
-      let value:string = ''
-      let inputTpl = html`<input type=${q.type} class="form-answer" data-question-uuid$="${q.uuid}" data-name$="${q.name}">`
-      if (submission) {
-        const answered = submission.answers.find(a => a.questionUuid === q.uuid)
-        if (answered) {
-          value = answered.value
-        }
-        inputTpl = html`
-            <input disabled value="${value}"
-             type=${q.type} class="form-answer">`
-      }
-    return html`
-    <div class="cell medium-12">
-      <label>${q.label}</label>
-      ${inputTpl}
-    </div>`
-  })
-
-
-  const submit = async (evt:Event, form:Form) => {
-    evt.preventDefault();
-    (el.querySelector('[type="submit"]') as HTMLButtonElement).disabled = true
-
+  const onSubmit = async (answers:Answer[], form:Form) => {
     const submission = <Submission>{
-      answers: collectAnswers(),
+      answers: answers,
       uuid: uuid(),
       created: new Date(),
       formUuid: form.uuid,
@@ -80,46 +58,13 @@ export async function update () {
       }),
     })
 
-    el.querySelector('.confirmation-text').classList.remove('hide')
-    el.querySelector('.into-text').classList.add('hide')
+    return (res.status < 300)
   }
 
-  const tpl = html`
-  <h2>${form.name}</h2>
-
-  <div class="callout success confirmation-text hide">
-      <h5 class="">${form.confirmationText || "Thanks, we've recieved your answers!"}</h5>
-  </div>
-  <div class="callout primary into-text">
-      <h5 class="">${form.introText}</h5>
-  </div>
-
-  <form class="grid-x grid-margin-y" on-submit="${(evt:Event) => submit(evt, form)}">
-      ${questions}
-
-      ${submission ? null : html`<div class="cell small-12">
-          <button type="submit" class="button submit-button">Submit</button>
-      </div>`}
-  </form>
-  `
-
-  render(tpl, el)
-
-  Array.from(document.querySelectorAll('input'))
-    .filter((el:HTMLInputElement) => el.getAttribute('required') !== "required")
-    .forEach((el:HTMLInputElement) => el.addEventListener('blur', () => el.setAttribute('required','required'))
-  )
+  renderForm({
+    form,
+    submission,
+    onSubmit,
+  }, el)
 }
 
-function collectAnswers ():Answer[] {
-  const answers:Answer[] = Array.from(document.querySelectorAll('.form-answer'))
-    .map((el:HTMLInputElement) => {
-    return <Answer>{
-      value: el.value,
-      name: el.getAttribute('data-name'),
-      questionUuid: el.getAttribute('data-question-uuid'),
-    }
-  })
-
-  return answers
-}
