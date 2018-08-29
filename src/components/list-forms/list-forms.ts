dc812f3ba725c2acb3a2878433d35fea3a2adc30
julian <github2018@studio42.dk>
2018-08-29 16:02:26 +0200

    visual tweaks

--- src/components/list-forms/list-forms.ts
+++ src/components/list-forms/list-forms.ts
@@ -30,6 +30,8 @@ export async function init () {
       .filter(([map]) => Object.values(map).length > 0)
       .map(([map, row]) => {
         row.submissions = Object.values(map).length
+        row.views = -1
+        row.replyRatio = -1
         return row
       })
 
@@ -44,34 +46,58 @@ export async function init () {
 export function update () {
   const {listView} = Store.store
 
-  const formsListTpl = listView
+  const tpl = html`
+  <h3>Your forms (${listView.length})</h3>
+  
+  <!--headers-->
+  <div class="grid-x grid-margin-y">
+      <div class="cell small-4">
+          <small>Name</small>
+      </div>
+      <div class="cell small-1">
+          <small>Views</small>
+      </div>
+      <div class="cell small-2">
+          <small>Submissions</small>
+      </div>
+      <div class="cell small-1">
+          <small>Ratio</small>
+      </div>
+      <div class="cell small-2">
+          <small>Created</small>
+      </div>
+      <div class="cell small-2 text-right">
+          <small>Actions</small>
+      </div>
+  </div>
+  <!--rows-->
+  ${listView
     .sort((a, b) => a.created.getTime() - b.created.getTime())
     .map(form => html`
-    <div class="grid-x grid-margin-x grid-margin-y">
-      <div class="cell auto">
+    <div class="grid-x grid-margin-y">
+      <div class="cell medium-4 small-12" style="overflow: hidden;white-space: nowrap">
           ${form.name}
       </div>
-      <div class="cell shrink">
+      <div class="cell small-1 small-offset-4 hide-for-medium">      
           ${form.views}
       </div>
-      <div class="cell shrink">
+        <div class="cell small-1 hide-for-small-only">      
+            ${form.views}
+        </div>
+      <div class="cell small-2">
           ${form.submissions}
       </div>
-      <div class="cell shrink">
-          ${form.replyRatio}
+      <div class="cell small-1">
+          ${form.replyRatio}%
       </div>
-      <div class="cell shrink">
+      <div class="cell small-2">
           ${formatDate(form.created)}
       </div>
-      <div class="cell shrink">
+      <div class="cell small-2 text-right">
           <button class="clear button link" on-click="${() => Store.setRouteAction(Route.FormView, {formId: form.uuid}) }">View</button>
       </div>
     </div>
-    `)
-
-  const tpl = html`
-  <h3>Your forms (${listView.length})</h3>
-  ${formsListTpl}
+    `)}
   `
   const el:HTMLElement = document.querySelector('forms-list')
   render(tpl, el)
