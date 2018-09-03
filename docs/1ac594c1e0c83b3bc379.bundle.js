2af30f206a625b2257c5ccd0ba7edcb68052acfe
julian <github2018@studio42.dk>
2018-09-03 12:42:14 +0200

    build

--- docs/1ac594c1e0c83b3bc379.bundle.js
+++ docs/1ac594c1e0c83b3bc379.bundle.js
@@ -50169,6 +50169,206 @@ module.exports = EVP_BytesToKey
 
 /***/ }),
 
+/***/ "./node_modules/file-saver/FileSaver.js":
+/*!**********************************************!*\
+  !*** ./node_modules/file-saver/FileSaver.js ***!
+  \**********************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+var __WEBPACK_AMD_DEFINE_RESULT__;/* FileSaver.js
+ * A saveAs() FileSaver implementation.
+ * 1.3.2
+ * 2016-06-16 18:25:19
+ *
+ * By Eli Grey, http://eligrey.com
+ * License: MIT
+ *   See https://github.com/eligrey/FileSaver.js/blob/master/LICENSE.md
+ */
+
+/*global self */
+/*jslint bitwise: true, indent: 4, laxbreak: true, laxcomma: true, smarttabs: true, plusplus: true */
+
+/*! @source http://purl.eligrey.com/github/FileSaver.js/blob/master/FileSaver.js */
+
+var saveAs = saveAs || (function(view) {
+	"use strict";
+	// IE <10 is explicitly unsupported
+	if (typeof view === "undefined" || typeof navigator !== "undefined" && /MSIE [1-9]\./.test(navigator.userAgent)) {
+		return;
+	}
+	var
+		  doc = view.document
+		  // only get URL when necessary in case Blob.js hasn't overridden it yet
+		, get_URL = function() {
+			return view.URL || view.webkitURL || view;
+		}
+		, save_link = doc.createElementNS("http://www.w3.org/1999/xhtml", "a")
+		, can_use_save_link = "download" in save_link
+		, click = function(node) {
+			var event = new MouseEvent("click");
+			node.dispatchEvent(event);
+		}
+		, is_safari = /constructor/i.test(view.HTMLElement) || view.safari
+		, is_chrome_ios =/CriOS\/[\d]+/.test(navigator.userAgent)
+		, throw_outside = function(ex) {
+			(view.setImmediate || view.setTimeout)(function() {
+				throw ex;
+			}, 0);
+		}
+		, force_saveable_type = "application/octet-stream"
+		// the Blob API is fundamentally broken as there is no "downloadfinished" event to subscribe to
+		, arbitrary_revoke_timeout = 1000 * 40 // in ms
+		, revoke = function(file) {
+			var revoker = function() {
+				if (typeof file === "string") { // file is an object URL
+					get_URL().revokeObjectURL(file);
+				} else { // file is a File
+					file.remove();
+				}
+			};
+			setTimeout(revoker, arbitrary_revoke_timeout);
+		}
+		, dispatch = function(filesaver, event_types, event) {
+			event_types = [].concat(event_types);
+			var i = event_types.length;
+			while (i--) {
+				var listener = filesaver["on" + event_types[i]];
+				if (typeof listener === "function") {
+					try {
+						listener.call(filesaver, event || filesaver);
+					} catch (ex) {
+						throw_outside(ex);
+					}
+				}
+			}
+		}
+		, auto_bom = function(blob) {
+			// prepend BOM for UTF-8 XML and text/* types (including HTML)
+			// note: your browser will automatically convert UTF-16 U+FEFF to EF BB BF
+			if (/^\s*(?:text\/\S*|application\/xml|\S*\/\S*\+xml)\s*;.*charset\s*=\s*utf-8/i.test(blob.type)) {
+				return new Blob([String.fromCharCode(0xFEFF), blob], {type: blob.type});
+			}
+			return blob;
+		}
+		, FileSaver = function(blob, name, no_auto_bom) {
+			if (!no_auto_bom) {
+				blob = auto_bom(blob);
+			}
+			// First try a.download, then web filesystem, then object URLs
+			var
+				  filesaver = this
+				, type = blob.type
+				, force = type === force_saveable_type
+				, object_url
+				, dispatch_all = function() {
+					dispatch(filesaver, "writestart progress write writeend".split(" "));
+				}
+				// on any filesys errors revert to saving with object URLs
+				, fs_error = function() {
+					if ((is_chrome_ios || (force && is_safari)) && view.FileReader) {
+						// Safari doesn't allow downloading of blob urls
+						var reader = new FileReader();
+						reader.onloadend = function() {
+							var url = is_chrome_ios ? reader.result : reader.result.replace(/^data:[^;]*;/, 'data:attachment/file;');
+							var popup = view.open(url, '_blank');
+							if(!popup) view.location.href = url;
+							url=undefined; // release reference before dispatching
+							filesaver.readyState = filesaver.DONE;
+							dispatch_all();
+						};
+						reader.readAsDataURL(blob);
+						filesaver.readyState = filesaver.INIT;
+						return;
+					}
+					// don't create more object URLs than needed
+					if (!object_url) {
+						object_url = get_URL().createObjectURL(blob);
+					}
+					if (force) {
+						view.location.href = object_url;
+					} else {
+						var opened = view.open(object_url, "_blank");
+						if (!opened) {
+							// Apple does not allow window.open, see https://developer.apple.com/library/safari/documentation/Tools/Conceptual/SafariExtensionGuide/WorkingwithWindowsandTabs/WorkingwithWindowsandTabs.html
+							view.location.href = object_url;
+						}
+					}
+					filesaver.readyState = filesaver.DONE;
+					dispatch_all();
+					revoke(object_url);
+				}
+			;
+			filesaver.readyState = filesaver.INIT;
+
+			if (can_use_save_link) {
+				object_url = get_URL().createObjectURL(blob);
+				setTimeout(function() {
+					save_link.href = object_url;
+					save_link.download = name;
+					click(save_link);
+					dispatch_all();
+					revoke(object_url);
+					filesaver.readyState = filesaver.DONE;
+				});
+				return;
+			}
+
+			fs_error();
+		}
+		, FS_proto = FileSaver.prototype
+		, saveAs = function(blob, name, no_auto_bom) {
+			return new FileSaver(blob, name || blob.name || "download", no_auto_bom);
+		}
+	;
+	// IE 10+ (native saveAs)
+	if (typeof navigator !== "undefined" && navigator.msSaveOrOpenBlob) {
+		return function(blob, name, no_auto_bom) {
+			name = name || blob.name || "download";
+
+			if (!no_auto_bom) {
+				blob = auto_bom(blob);
+			}
+			return navigator.msSaveOrOpenBlob(blob, name);
+		};
+	}
+
+	FS_proto.abort = function(){};
+	FS_proto.readyState = FS_proto.INIT = 0;
+	FS_proto.WRITING = 1;
+	FS_proto.DONE = 2;
+
+	FS_proto.error =
+	FS_proto.onwritestart =
+	FS_proto.onprogress =
+	FS_proto.onwrite =
+	FS_proto.onabort =
+	FS_proto.onerror =
+	FS_proto.onwriteend =
+		null;
+
+	return saveAs;
+}(
+	   typeof self !== "undefined" && self
+	|| typeof window !== "undefined" && window
+	|| this.content
+));
+// `self` is undefined in Firefox for Android content script context
+// while `this` is nsIContentFrameMessageManager
+// with an attribute `content` that corresponds to the window
+
+if (typeof module !== "undefined" && module.exports) {
+  module.exports.saveAs = saveAs;
+} else if (("function" !== "undefined" && __webpack_require__(/*! !webpack amd define */ "./node_modules/webpack/buildin/amd-define.js") !== null) && (__webpack_require__(/*! !webpack amd options */ "./node_modules/webpack/buildin/amd-options.js") !== null)) {
+  !(__WEBPACK_AMD_DEFINE_RESULT__ = (function() {
+    return saveAs;
+  }).call(exports, __webpack_require__, exports, module),
+				__WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+}
+
+
+/***/ }),
+
 /***/ "./node_modules/form-data/lib/browser.js":
 /*!***********************************************!*\
   !*** ./node_modules/form-data/lib/browser.js ***!
@@ -99722,6 +99922,34 @@ exports.createContext = Script.createContext = function (context) {
 
 /***/ }),
 
+/***/ "./node_modules/webpack/buildin/amd-define.js":
+/*!***************************************!*\
+  !*** (webpack)/buildin/amd-define.js ***!
+  \***************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+module.exports = function() {
+	throw new Error("define cannot be used indirect");
+};
+
+
+/***/ }),
+
+/***/ "./node_modules/webpack/buildin/amd-options.js":
+/*!****************************************!*\
+  !*** (webpack)/buildin/amd-options.js ***!
+  \****************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+/* WEBPACK VAR INJECTION */(function(__webpack_amd_options__) {/* globals __webpack_amd_options__ */
+module.exports = __webpack_amd_options__;
+
+/* WEBPACK VAR INJECTION */}.call(this, {}))
+
+/***/ }),
+
 /***/ "./node_modules/webpack/buildin/global.js":
 /*!***********************************!*\
   !*** (webpack)/buildin/global.js ***!
@@ -101452,13 +101680,33 @@ exports.codeBases = {
     tasksViewCounter: 'https://raw.githubusercontent.com/FoundersAS/dappform-tasks-view-counter/master/package.json',
 };
 async function deployTasks() {
-    settings.setValue('hostingTaskUrl', (await webtask_1.createWebTaskTask('dappform-tasks-host', "https://raw.githubusercontent.com/FoundersAS/dappform-tasks-form-hosting/master/index.js", "https://raw.githubusercontent.com/FoundersAS/dappform-tasks-form-hosting/master/package.json", {})).webtask_url);
-    settings.setValue('tasksViewCounter', (await webtask_1.createWebTaskTask('dappform-tasks-view-counter', "https://raw.githubusercontent.com/FoundersAS/dappform-tasks-view-counter/master/index.js", "https://raw.githubusercontent.com/FoundersAS/dappform-tasks-view-counter/master/package.json", Object.assign({}, blockstackUtils_1.default.getBlockstackLocalStorage()))).webtask_url);
-    settings.setValue('submissionTaskUrl', (await webtask_1.createWebTaskTask('dappform-tasks-submission', 'https://raw.githubusercontent.com/FoundersAS/dappform-tasks-submissions/master/index.js', 'https://raw.githubusercontent.com/FoundersAS/dappform-tasks-submissions/master/package.json', Object.assign({}, blockstackUtils_1.default.getBlockstackLocalStorage(), { BLOCKSTACK_APP_PRIVATE_KEY: new blockstackUtils_1.default().privateKey }))).webtask_url);
-    settings.setValue('statsTaskUrl', (await webtask_1.createWebTaskTask('dappform-tasks-stats', 'https://raw.githubusercontent.com/FoundersAS/dappform-tasks-stats/master/index.js', 'https://raw.githubusercontent.com/FoundersAS/dappform-tasks-stats/master/package.json', Object.assign({}, blockstackUtils_1.default.getBlockstackLocalStorage(), { POSTMARK_TOKEN: settings.getValue('postmarkToken'), POSTMARK_FROM: settings.getValue('postmarkFrom'), POSTMARK_TO: settings.getValue('email') }))).webtask_url);
-    await webtask_1.createCronSchedule('dappform-tasks-stats', settings.getValue('cronSchedule'));
-    await settings.saveSettings();
-    update();
+    webtask_1.createWebTaskTask('dappform-tasks-host', "https://raw.githubusercontent.com/FoundersAS/dappform-tasks-form-hosting/master/index.js", "https://raw.githubusercontent.com/FoundersAS/dappform-tasks-form-hosting/master/package.json", {})
+        .then(result => settings.setValue('hostingTaskUrl', result.webtask_url)).catch(console.error)
+        .then(() => Save.scheduleSave());
+    webtask_1.createWebTaskTask('dappform-tasks-host', "https://raw.githubusercontent.com/FoundersAS/dappform-tasks-form-hosting/master/index.js", "https://raw.githubusercontent.com/FoundersAS/dappform-tasks-form-hosting/master/package.json", {}).then(result => settings.setValue('hostingTaskUrl', result.webtask_url)).catch(console.error)
+        .then(() => Save.scheduleSave());
+    webtask_1.createWebTaskTask('dappform-tasks-view-counter', "https://raw.githubusercontent.com/FoundersAS/dappform-tasks-view-counter/master/index.js", "https://raw.githubusercontent.com/FoundersAS/dappform-tasks-view-counter/master/package.json", Object.assign({}, blockstackUtils_1.default.getBlockstackLocalStorage())).then(result => settings.setValue('tasksViewCounter', result.webtask_url)).catch(console.error)
+        .then(() => Save.scheduleSave());
+    webtask_1.createWebTaskTask('dappform-tasks-submission', 'https://raw.githubusercontent.com/FoundersAS/dappform-tasks-submissions/master/index.js', 'https://raw.githubusercontent.com/FoundersAS/dappform-tasks-submissions/master/package.json', Object.assign({}, blockstackUtils_1.default.getBlockstackLocalStorage(), { BLOCKSTACK_APP_PRIVATE_KEY: new blockstackUtils_1.default().privateKey }))
+        .then(result => settings.setValue('submissionTaskUrl', result.webtask_url)).catch(console.error)
+        .then(() => Save.scheduleSave());
+    webtask_1.createWebTaskTask('dappform-tasks-stats', 'https://raw.githubusercontent.com/FoundersAS/dappform-tasks-stats/master/index.js', 'https://raw.githubusercontent.com/FoundersAS/dappform-tasks-stats/master/package.json', Object.assign({}, blockstackUtils_1.default.getBlockstackLocalStorage(), { POSTMARK_TOKEN: settings.getValue('postmarkToken'), POSTMARK_FROM: settings.getValue('postmarkFrom'), POSTMARK_TO: settings.getValue('email') })).then(result => settings.setValue('statsTaskUrl', result.webtask_url)).catch(console.error)
+        .then(() => Save.scheduleSave());
+    webtask_1.createCronSchedule('dappform-tasks-stats', settings.getValue('cronSchedule'))
+        .catch(console.error);
+}
+class Save {
+    static async scheduleSave(delay = 1) {
+        if (!Save.scheduled) {
+            Save.scheduled = true;
+            await wait(delay);
+            await Promise.all([update(), settings.saveSettings()]);
+            Save.scheduled = false;
+        }
+    }
+}
+function wait(sec) {
+    return new Promise(resolve => setTimeout(() => resolve(), sec * 1000));
 }
 async function saveUserDefinedSettings() {
     Object.entries(settings.settingsSchema)
@@ -101666,6 +101914,7 @@ const store_1 = __webpack_require__(/*! ../../store */ "./src/store.ts");
 const router_1 = __webpack_require__(/*! ../router */ "./src/components/router.ts");
 const dappform_forms_api_1 = __webpack_require__(/*! dappform-forms-api */ "./node_modules/dappform-forms-api/dist/index.js");
 const write_1 = __webpack_require__(/*! ../../util/write */ "./src/util/write.ts");
+const FileSaver = __webpack_require__(/*! file-saver */ "./node_modules/file-saver/FileSaver.js");
 const extMimeMap = new Map([
     ['txt', 'text/plain'],
     ['csv', 'text/csv'],
@@ -101676,19 +101925,16 @@ const extMimeMap = new Map([
     ['xml', 'application/xml'],
     ['xls', 'application/vnd.ms-excel'],
 ]);
-async function getDataUrl(url) {
+async function getBlob(url) {
     const ext = url.substr(url.lastIndexOf(".") + 1);
     const path = url.substr(url.indexOf('files/'));
     let buffer = await write_1.getAnyFile(path);
-    if (!buffer)
-        return url;
-    const enc = new TextDecoder();
-    const decoded = enc.decode(buffer);
-    const blob = new Blob([...decoded], { type: extMimeMap.get(ext) });
-    return URL.createObjectURL(blob);
+    const blob = new Blob([buffer], { type: extMimeMap.get(ext) });
+    return { blob, ext };
 }
 async function download(fileLink) {
-    location.href = await getDataUrl(fileLink);
+    const { blob, ext } = await getBlob(fileLink);
+    FileSaver.saveAs(blob, `attachment.${ext}`);
 }
 async function update() {
     const el = document.querySelector('submissions-view');
@@ -101954,19 +102200,26 @@ function getCronUrl(taskName) {
     return `https://sandbox.auth0-extend.com/api/cron/${settings.getValue('webtaskId')}/${taskName}?key=${settings.getValue('webtaskToken')}`;
 }
 async function createWebTaskTask(taskName, taskCodeUrl, taskPackageUrl, taskSecrets = {}) {
-    const p = await fetch(taskPackageUrl).then((res) => { return res.json(); });
-    taskSecrets.version = p.version;
-    console.debug(`Deploying ${taskName} v${p.version}`);
-    return (await fetch(getTaskUrl(taskName), {
+    let res;
+    res = await fetch(taskPackageUrl);
+    const packageJson = await res.json();
+    taskSecrets.version = packageJson.version;
+    console.debug(`Deploying ${taskName} v${packageJson.version}`);
+    res = await fetch(getTaskUrl(taskName), {
         method: 'PUT',
         body: JSON.stringify({
             url: taskCodeUrl,
             secrets: taskSecrets,
             meta: {
-                'wt-node-dependencies': JSON.stringify(p.dependencies).replace(/\^/g, '')
+                'wt-node-dependencies': JSON.stringify(packageJson.dependencies).replace(/\^/g, '') // remove ^ because webtask doesn't support it
             }
         })
-    }).then((res) => res.json()));
+    });
+    const deployed = await res.json();
+    if (res.status > 299) {
+        throw deployed; // error
+    }
+    return deployed;
 }
 exports.createWebTaskTask = createWebTaskTask;
 async function createCronSchedule(taskName, schedule) {
@@ -102105,4 +102358,4 @@ exports.getAnyFile = getAnyFile;
 /***/ })
 
 /******/ });
-//# sourceMappingURL=a2f67e5d698e84f91948.bundle.js.map
+//# sourceMappingURL=1ac594c1e0c83b3bc379.bundle.js.map
